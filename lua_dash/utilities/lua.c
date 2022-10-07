35b0154850a673bfb1b379da588b3ab5d9beaa4a
Kevin Blanchard <kevin.blanchard@menlosecurity.com>
2022-10-07 18:56:38 -0400

    Add in some refactoring

---
+++ lua_dash/utilities/lua.c
@@ -0,0 +1,12 @@
+#include <lua.h>
+#include <lauxlib.h>
+#include "lua.h"
+#include "../debug/debug.h"
+
+void GetLuaTableSg(lua_State* state, const char* table_name)
+{
+    lua_getglobal(state, table_name);
+    if(!lua_istable(state, -1))
+        LogError("This isn't a table");
+
+}
---
+++ lua_dash/utilities/lua.h
@@ -0,0 +1,6 @@
+#pragma once
+
+struct lua_State;
+typedef struct lua_State lua_State;
+
+void GetLuaTableSg(lua_State* state, const char* table_name);
