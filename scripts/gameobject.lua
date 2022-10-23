edf769f18be3e7786a3fa9b527057ac982b053c0
Kevin Blanchard <kevin.blanchard@menlosecurity.com>
2022-10-23 19:34:17 -0400

    Add some comments, clean some stuff

--- scripts/gameobject.lua
+++ scripts/gameobject.lua
@@ -1,4 +1,5 @@
 local initialize = require "GameObject"
+--Aux is the C "auxiliary" library, where we call these from C
 local aux,prv = {},{}
 local GameObject = {
     userdata = {},
@@ -7,27 +8,40 @@ local GameObject = {
 }
 
 prv = initialize(aux,prv)
+--Userdata metatable.  Could be used to set the C userdata stuff in lua if we want.
 local umeta = {}
 --umeta.__gc = prv.del
 local gameobjects = setmetatable({},{__mode = "v"})
+--Use the GameObject to look things up.
 GameObject.__index = GameObject
 
+--Gets the X value of a gameobject.
 function GameObject:x()
     local x = prv.X(self.userdata)
-    print("X value in lua is "..x)
+    return x
+end
+
+--Gets the Y value of a gameobject
+function GameObject:y()
+    local x = prv.Y(self.userdata)
     return x
 end
 
 function GameObject:location()
-    return prv.Location()
+    local x,y =  prv.Location()
+    return x,y
 end
 
+--The function that is called when a gameobject is started, this should be overrode if a gameobject needs to update.
 function GameObject:start()
-    return 0
 end
 
---global functions
-function Create_gameobject(x, y, start,lua_obj)
+--The function that is called when a gameobject is updated every frame, this should be overrode if a gameobject needs to update.
+function GameObject:update()
+end
+
+--global functions, like creating this object
+function Create_gameobject(lua_obj,x, y, start, update)
     local gameobject  = {}
     setmetatable(gameobject,GameObject)
     local udata, ptr = prv.Create(x,y)
@@ -35,16 +49,18 @@ function Create_gameobject(x, y, start,lua_obj)
     gameobject.ptr = ptr
     gameobjects[ptr] = lua_obj
     gameobject.start = start or GameObject.start
+    gameobject.update = update or GameObject.update
     return gameobject, ptr
 end
 
+--Function that is meant to be called from C, which will start or update the gameobject.
 function aux.start_gameobject(ptr)
     local gameobj = gameobjects[ptr]
     gameobj.gameobject.start(gameobj)
 end
 
 
---Gameobject userdata metatable - Used by "create" c function
+--Gameobject userdata metatable - Used by "create" c function, to add in the proper metatable if we want.
 aux.metatable = umeta
 
 return GameObject
